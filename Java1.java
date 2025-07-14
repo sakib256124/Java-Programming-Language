51bca47db8d07b800fdf7b6291934a16de6c4fd0
Sakib Hassan <mdsakibhassan256124@gmail.com>
2025-07-15 00:40:01 +0600

    First revison

---
+++ Java1.java
@@ -0,0 +1,9 @@
+public class Java1 {
+
+    public static void main(String[] args) {
+        System.out.println("Interger size " + Integer.BYTES + " Bit " + Integer.SIZE);
+        System.out.println("char size " + Byte.BYTES + " Bit " + Byte.SIZE);
+        System.out.println("Double size " + Double.BYTES + " Bit " + Double.SIZE);
+        System.out.println("Float size " + Integer.BYTES + " Bit " + Float.SIZE);
+    }
+}
