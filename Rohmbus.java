ea67f7944aa2cfc713347d72a306198dca1194e4
Sakib Hassan <mdsakibhassan256124@gmail.com>
2025-07-23 21:12:46 +0600

    Pattern

---
+++ Rohmbus.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Rohmbus {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=(n-i);j++){
+                System.out.print(" ");
+            }for(int j = 1;j<=n;j++){
+            if( i ==1 || i ==n || j ==1 || j ==n){
+                
+                System.out.print("*");
+            } else{
+                System.out.print(" ");
+            }
+            }
+           
+            System.out.println();
+        }
+    }
+    
+}
