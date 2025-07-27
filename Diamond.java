396abff9c58a988a40f108f6599d687c9c207267
Sakib Hassan <mdsakibhassan256124@gmail.com>
2025-07-27 17:39:27 +0600

    Patterns

---
+++ Diamond.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Diamond {
+    public static void main(String[] args) {
+        int n ;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k = 1;k<=i;k++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
